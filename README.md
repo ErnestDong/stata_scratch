8b548a968b939a7d2313be0fb020ef430002d3f8
dcy <dongcylyj@163.com>
2020-03-30 15:44:13 +0800

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Stata by Python
+main.py文件利用flask框架搭建了一个网页，计划网页的根目录作为首页传递用户上传的文件，/result返回试验结果,/error错误处理
---
+++ main.py
@@ -0,0 +1,52 @@
+
+from flask import Flask, request,make_response,render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+from os import path
+app = Flask(__name__)
+@app.route("/",methods=['GET','POST'])
+def upload():
+    try:
+        if request.method=='POST':
+            f = request.files["file"]
+            base_path = path.abspath(path.dirname(__file__))
+            upload_path = path.join(base_path,'static/uploads/')
+            file_name = upload_path + secure_filename(f.filename)
+            f.save(file_name)
+            return redirect("result")
+        return render_template('upload.html')
+    except:
+        return redirect("error")
+
+@app.route("/result")
+def getResult():
+    return """
+0
+"""
+
+@app.route("/error")
+def cerr():
+    html="""
+<!DOCTYPE html>
+<html lang="zh">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Title</title>
+  </head>
+  <body>
+  <script>
+  alert("Please Check Your Data!")
+  </script>
+    <form action="" method="post" enctype="multipart/form-data">
+      <select name="model_id">
+        <option value="0">OLS</option>
+        <option value="1">ML</option>
+      </select>
+      <input type="file" name="file" />
+      <input type="submit" value="upload" />
+    </form>
+  </body>
+</html>
+    """
+    return html
+if __name__=="__main__":
+    app.run()
