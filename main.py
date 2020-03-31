9b21791a8a9b38dc2ad1607fd126c732a8e56c70
dcy <dongcylyj@163.com>
2020-03-31 18:56:09 +0800

    remove extra files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request,make_response,render_template, redirect, url_for
+from flask import Flask, request,make_response,render_template, redirect
 from werkzeug.utils import secure_filename
 from os import path
 app = Flask(__name__)
