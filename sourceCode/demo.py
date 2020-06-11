7abe2233f5ecfbf60cff3c4667d5ca8fee440261
dcy <dongcylyj@gmail.com>
2020-06-11 23:44:03 +0800

    add demo

---
+++ sourceCode/demo.py
@@ -0,0 +1,14 @@
+def getAns(dependent: str, independent: list, session: dict) -> dict:
+    ans={}
+    return ans
+
+
+def showAns(dependent: str, ans: dict, session: dict) -> str:
+    return "<center><p>Hello World!</h>"
+
+def showFigure(ans: dict) -> dict:
+    return {"demo.png":"/static/pic/demo.png"}
+
+
+if __name__ == "__main__":
+    print(getAns("open", ["amount", "low"], "dcy"))
